9ea3027e0cdab89c9b4b8b5f1a00d48e87a2482a
VINICIUS MANIEZO ALVES <42972208811@info.local>
2026-09-08 14:56:47 -0300

    :sparkles: feat: Adicionado feature para ver senha

--- login/login.js
+++ login/login.js
@@ -12,10 +12,20 @@
 // Atribui um valor para variavel "campoSenha" puxando o elemento do html pelo id "senha"
 const campoSenha = document.getElementById("senha")
 // Atribui um valor para a variavel "btnSenha" utilizando class ou id indicados por . ou #
-const btnSenha =document.querySelector("#show-password")
+const btnSenha = document.querySelector("#show-password")
+
+console.log(btnSenha.type)
 
 // Ele espera o acontecimento de um evento, sendo o "click" do mouse e realizando uma função
-btnSenha.addEventListener("click", function() {
-    alert("Cliquei no botão!");
-    
+btnSenha.addEventListener("click", function () {
+
+    // if (campoSenha.type == "password") {
+    //     campoSenha.type = "text";
+    // }
+    // else {
+    //     campoSenha.type = "password"
+    // }
+
+    // campoSenha.type recebe uma pergunta: o tipo do campoSenha é uma senha? se sim recebe "text" se não recebe "password"
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password"
 })
